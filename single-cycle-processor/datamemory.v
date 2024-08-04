9adc65c361c621ac969df2a9be8c8c7af392bc59
ashwinnair10 <ashwinnair1894@gmail.com>
2024-08-04 18:42:44 +0530

    added data memory and instruction memory

--- single-cycle-processor/datamemory.v
+++ single-cycle-processor/datamemory.v
@@ -3,8 +3,41 @@ module datamemory(
     input[31:0] address,wdata,
     output reg[31:0] op
 );
-    reg [31:0]data[63:0];
-    //declare data here
+    reg [31:0]data[31:0];
+    initial begin
+        data[0]='hxxxxxxxxxxxxxxxx;
+        data[1]='hxxxxxxxxxxxxxxxx;
+        data[2]='hxxxxxxxxxxxxxxxx;
+        data[3]='hxxxxxxxxxxxxxxxx;
+        data[4]='hxxxxxxxxxxxxxxxx;
+        data[5]='hxxxxxxxxxxxxxxxx;
+        data[6]='hxxxxxxxxxxxxxxxx;
+        data[7]='hxxxxxxxxxxxxxxxx;
+        data[8]='hxxxxxxxxxxxxxxxx;
+        data[9]='hxxxxxxxxxxxxxxxx;
+        data[10]='hxxxxxxxxxxxxxxxx;
+        data[11]='hxxxxxxxxxxxxxxxx;
+        data[12]='hxxxxxxxxxxxxxxxx;
+        data[13]='hxxxxxxxxxxxxxxxx;
+        data[14]='hxxxxxxxxxxxxxxxx;
+        data[15]='hxxxxxxxxxxxxxxxx;
+        data[16]='hxxxxxxxxxxxxxxxx;
+        data[17]='hxxxxxxxxxxxxxxxx;
+        data[18]='hxxxxxxxxxxxxxxxx;
+        data[19]='hxxxxxxxxxxxxxxxx;
+        data[20]='hxxxxxxxxxxxxxxxx;
+        data[21]='hxxxxxxxxxxxxxxxx;
+        data[22]='hxxxxxxxxxxxxxxxx;
+        data[23]='hxxxxxxxxxxxxxxxx;
+        data[24]='hxxxxxxxxxxxxxxxx;
+        data[25]='hxxxxxxxxxxxxxxxx;
+        data[26]='hxxxxxxxxxxxxxxxx;
+        data[27]='hxxxxxxxxxxxxxxxx;
+        data[28]='hxxxxxxxxxxxxxxxx;
+        data[29]='hxxxxxxxxxxxxxxxx;
+        data[30]='hxxxxxxxxxxxxxxxx;
+        data[31]='hxxxxxxxxxxxxxxxx;
+    end
     always @(*)begin
         if(memwrite==1'b1)
             data[address[31:2]]=wdata;
--- single-cycle-processor/instrmemory.v
+++ single-cycle-processor/instrmemory.v
@@ -2,8 +2,40 @@ module instrmemory(
     input[31:0] addr,
     output reg[31:0] instr
 );
-    reg [31:0]instruction[63:0];
-    //declare instructions here
+    wire [31:0]instruction[31:0];
+    assign instruction[0]='h20020014;
+    assign instruction[1]='h2003001e;
+    assign instruction[2]='h20050000;
+    assign instruction[3]='h10a00001;
+    assign instruction[4]='h00432820;
+    assign instruction[5]='hac050014;
+    assign instruction[6]='h00000000;
+    assign instruction[7]='h00000000;
+    assign instruction[8]='h00000000;
+    assign instruction[9]='h00000000;
+    assign instruction[10]='h00000000;
+    assign instruction[11]='h00000000;
+    assign instruction[12]='h00000000;
+    assign instruction[13]='h00000000;
+    assign instruction[14]='h00000000;
+    assign instruction[15]='h00000000;
+    assign instruction[16]='h00000000;
+    assign instruction[17]='h00000000;
+    assign instruction[18]='h00000000;
+    assign instruction[19]='h00000000;
+    assign instruction[20]='h00000000;
+    assign instruction[21]='h00000000;
+    assign instruction[22]='h00000000;
+    assign instruction[23]='h00000000;
+    assign instruction[24]='h00000000;
+    assign instruction[25]='h00000000;
+    assign instruction[26]='h00000000;
+    assign instruction[27]='h00000000;
+    assign instruction[28]='h00000000;
+    assign instruction[29]='h00000000;
+    assign instruction[30]='h00000000;
+    assign instruction[31]='h00000000;
+
     always @(*)begin
         instr=instruction[addr[31:2]];
     end
