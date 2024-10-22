7d6579b673388a7b11350524f5c893e3066ebc23
ashwinnair10 <ashwinnair1894@gmail.com>
2024-10-22 14:28:58 +0530

    update

--- multi-cycle-processor/sl.v
+++
@@ -1,6 +0,0 @@
-module sl(
-    input[31:0] a,
-    output[31:0] b
-);
-    assign b={a[29:0],2'b00};
-endmodule
--- single-cycle-processor/shiftleft.v
+++
@@ -1,8 +0,0 @@
-module shiftleft(
-    input[31:0] ip,
-    output reg[31:0] op
-);
-    always @(*)begin
-        op=ip<<2;
-    end
-endmodule
